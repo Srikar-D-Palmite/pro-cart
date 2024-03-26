755ec3d1b7f80e5de29f95afcd0b31438baa42ed
Srikar-D-Palmite <palmite@Laptop-Srikar>
2024-03-26 23:34:00 +0530

    Simple homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -8,8 +9,8 @@ const App = () => {
     <>
       <Header />
       <main className="py-3">
-      <Container>
-        <h1>Welcome to ProCart</h1>
+        <Container>
+          <Outlet />
         </Container>
       </main>
       <Footer />
