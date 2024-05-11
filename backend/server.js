59a773b6046b43f4fb88874751024f4e67ea2bc1
Srikar <t1srikar@gmail.com>
2024-05-11 11:45:30 +0530

    Using controllers

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 const port = process.env.PORT || 5000;
 
@@ -14,6 +15,9 @@ app.get('/', function(req, res) {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 // Tells server to listen on a port. Without this, you cannot access the API
 app.listen(port, () => console.log(`Server is running on port ${port}`));
